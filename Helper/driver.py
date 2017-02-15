d4d0a69dbd4ef9edb15a7b546dcb41aabf268390
Vaibhav Patil <vaibhav0422@gmail.com>
2017-02-14 23:05:16 -0500

    * Driver.py : Adding Support for firefox|| Login.py : Reading Values || xpathList : Adding more xpaths to the list

--- Helper/driver.py
+++ Helper/driver.py
@@ -1,6 +1,12 @@
+#Modify this file for setting the driver for diffferent browsers
+
 from selenium import webdriver 
 # from selenium.webdriver.common.keys import Keys
 # import re
 
-chrome_path = r"C:\Users\Vaibhav\Downloads\sikuli\chromedriver_win32(1)\chromedriver.exe"
+#Chrome Driver
+chrome_path = r"C:\Users\Vaibhav\Downloads\sikuli\chromedriver_win32(1)\chromedriver.exe" #My path to chromedriver.exe
 driver=webdriver.Chrome(chrome_path)
+
+#Firefox Driver
+#driver = webdriver.Firefox()
--- Login.py
+++ Login.py
@@ -5,22 +5,20 @@ Created on Feb 12, 2017
 '''
 from selenium.webdriver import chrome
 
-if __name__ == '__main__':
-    pass
-
 from Helper.driver import *
 from Helper.xpathList import *
 from Helper.credentials import *
+from time import sleep
 
 driver.get("https://www.instagram.com")
 # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Log In')))
 driver.find_element_by_xpath(loginButtonText).click()
 driver.find_element_by_xpath(userName).send_keys(uname)
 driver.find_element_by_xpath(password).send_keys(pwd)
+print("Forced wait 2 Seconds")
+sleep(2)
 driver.find_element_by_xpath(loginButton).click()
-delay = 3
-driver.get("https://www.instagram.com/photato_head")
-
-
-# elem = browser.find_element_by_link_text("Home")
-# elem.click()
+sleep(3)
+driver.get("https://www.instagram.com/photato_head") #<- My userName
+followersNumber = driver.find_element_by_xpath(followers).get_attribute("title") #<- Get the follower Number.
+print(followersNumber)
