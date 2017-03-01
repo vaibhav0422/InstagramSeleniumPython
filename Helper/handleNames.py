f7cb5adbe23d35e00ec2d2f6155dcd433d69ad12
Vaibhav Patil <vaibhav0422@gmail.com>
2017-03-01 00:02:45 -0500

    Restructuring the Project / Adding Logic for following celebrities from handleNames.py > Handle Names

---
+++ Helper/handleNames.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 28, 2017
+
+@author: Vaibhav
+'''
+
+hNames = ["selenagomez","justintimberlake","emmawatson","davidbeckham","vancityreynolds",
+            "aliaabhatt","ddlovato","kourtneykardash","leomessi","therock","badgalriri","jlo",
+            "katyperry","cristiano","mileycyrus","khloekardashian","neymarjr","nickiminaj",
+            "kyliejenner","iamsrk","arianagrande","taylorswift","kimkardashian",
+            "beyonce","deepikapadukone","shraddhakapoor","akshaykumar","jacquelinef143", 
+            "thehughjackman","leonardodicaprio","priyankachopra","priyankachopra","priyankachopra",
+            "gigihadid","blakelively","zacefron"]
--- Helper/xpathList.py
+++ Helper/xpathList.py
@@ -1,6 +1,15 @@
+#Login Page elements
 loginButtonText = "//*/section/main/article/div/div[2]/p/a"
 userName = "//*/section/main/article/div/div[1]/div/form/div[1]/input"
 password = "//*/section/main/article/div/div[1]/div/form/div[2]/input"
 loginButton = "//*/section/main/article/div/div[1]/div/form/span/button"
+
+#Top Ribbon
 homeButton = "//*/section/nav/div/div/div[2]/div/div/div[1]/a/div"
 followers ="//*/section/main/article/header/div[2]/ul/li[2]/a/span"
+searchIcon = "//*/section/nav/div/div/div[2]/div/div/div[2]/a/div"
+
+#Search page
+searchBar = "//*/section/main/div/div/div/div/div/span[2]"
+
+followButton ="//*/section/main/article/header/div[2]/div[1]/span[2]/span[1]"
